ae59150934a9df25216e6df8e0dff2b50ca28a83
rsato <rsato@ridge-i.com>
2019-08-05 21:48:09 +0900

    add: setup.py

--- pytorchdl_gta5/__init__.py
+++ pytorchdl_gta5/__init__.py
@@ -1,3 +1,4 @@
 from pathlib import Path
+from .dataset import GTA5
 
 PROJECT_ROOT = Path(__file__).parents[1].absolute()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyTorchDL_GTA5',
+    version='1.0.0',
+    packages=['tests', 'pytorchdl_gta5'],
+    url='git@github.com:sarrrrry/PyTorchDL_GTA5.git',
+    license='',
+    author='sarrrrry',
+    author_email='sarrrrry.sarrrrry@gmail.com',
+    description=''
+)
